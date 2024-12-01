20f1b83576455052bd1b71f6cb883f0579d90a85
Daniel <Daniel2003@gmail.com>
2024-12-01 03:03:49 -0300

    Adicionando alteraçoes no index.html, pre consumo da pokeapi, separando fech da insersao de li

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
-function main(texto){
-    console.log(texto);
+const offset = 0;
+const limit = 10;
+
+function convertPokemon(pokemon){
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"alt="${pokemon.name}">
+        </div>
+    </li>    
+    `;
 }
 
-main('Ola');
+const pokemonList = document.getElementById('pokemonList');
+
+PokeAPI.getPokemons(offset,limit).then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemon).join(''); 
+
+
+    //const ListPoke = []
+    //for (let i = 0; i < pokemons.length; i++) {
+    //    const pokemon = pokemons[i];
+    //    ListPoke.push(convertPokemon(pokemon))
+    //}
+    //console.log(ListPoke);
+})
+  .catch((error) => console.error(error));
+
+  
