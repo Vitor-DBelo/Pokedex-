e34424011bb23db3f92bde5d493aec4174ca0da4
Daniel <Daniel2003@gmail.com>
2024-12-02 00:13:12 -0300

    Consumo e exibiçao da api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,52 @@
-const offset = 0;
-const limit = 10;
+const MaxPokemon = 151;
 
-function convertPokemon(pokemon){
-    return `
-    <li class="pokemon">
-        <span class="number">#001</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
-            </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"alt="${pokemon.name}">
-        </div>
-    </li>    
-    `;
-}
+let offset = 0;
+const limit = 5;
 
 const pokemonList = document.getElementById('pokemonList');
-
-PokeAPI.getPokemons(offset,limit).then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemon).join(''); 
+const LoadMoreButton = document.getElementById('LoadMore');
 
 
-    //const ListPoke = []
-    //for (let i = 0; i < pokemons.length; i++) {
-    //    const pokemon = pokemons[i];
-    //    ListPoke.push(convertPokemon(pokemon))
+
+function LoadPokemonIten(offset, limit){
+    pokeApi.getPokemons(offset, limit)
+    .then((pokemons = []) => {     // Usando "+=" adicionamos o novo conteúdo HTML ao existente, enquanto "=" sobrescreve todo o conteúdo anterior
+        pokemonList.innerHTML += pokemons.map((pokemon) => `       
+            <li class="pokemon ${pokemon.type}"> 
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('');
+    })
+
+    .catch((error) => console.error('Erro ao carregar Pokémon:', error));
+
+    //pokeApi.getPokemons().then((pokemons = []) => {
+    //    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    //    pokemonList.innerHTML += newHtml;
+    //});
+    
+}
+LoadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    LoadPokemonIten(offset, limit)
+    //const qtdRecordsNexPag = offset + limit;    if para adicionar limit
+//
+    //if(qtdRecordsNexPag >= MaxPokemon){
+    //    const newLimit = MaxPokemon - offset;
+    //    LoadPokemonIten(offset, newLimit)
+//
+    //    LoadMoreButton.parentElement.removeChild(LoadMoreButton);
+    //}else{
+    //    LoadPokemonIten(offset, limit)
     //}
-    //console.log(ListPoke);
+    
 })
-  .catch((error) => console.error(error));
 
-  
+LoadPokemonIten(offset, limit);
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,13 +1,32 @@
+const pokeApi = {};
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
 
-const PokeAPI = {
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.types = types;
+    pokemon.type = types[0]; // Tipo principal
 
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
 }
 
-PokeAPI.getPokemons = (offset = 0,limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then((pokeDetail) => convertPokeApiDetailToPokemon(pokeDetail));
+};
+
+pokeApi.getPokemons = (offset, limit ) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
-}
+        .then((jsonBody) => jsonBody.results) // Obtem a lista de Pokémon
+        .then((pokemons) => 
+            Promise.all(pokemons.map((pokemon) => pokeApi.getPokemonDetail(pokemon)))
+        ) // Busca os detalhes de cada Pokémon
+        .catch((error) => console.error('Erro ao obter Pokémon:', error));
+};
